19bdbe3c4db652f099849d28f5b19d32ed5b06cd
aksoy <aksoy.android@gmail.com>
2021-04-04 22:07:58 +0300

    Initial commit

---
+++ app/src/main/java/com/aksoyh/week5networkcalls/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.aksoyh.week5networkcalls.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aksoyh.week5networkcalls.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
